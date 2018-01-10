f7cc3ac7a43fe8984978fe9f6fa0b9bdebf1c649
James-Hague <jameshague3dartist@gmail.com>
2018-01-10 14:23:18 +0000

    First commit

--- src/test/java/DemoSiteTest.java
+++ src/test/java/DemoSiteTest.java
@@ -1,5 +1,6 @@
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -10,41 +11,44 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 import static junit.framework.TestCase.assertTrue;
 import static org.testng.AssertJUnit.assertEquals;
-//import com.relevantcodes.extentreports.ExtentReports;
 
 public class DemoSiteTest {
 
-    private static XSSFSheet ExcelWSheet;
-    private static XSSFWorkbook ExcelWBook;
-    private static XSSFCell Cell;
-    private static XSSFRow Row;
-    public WebDriver driver;
-    public WebDriverWait wait;
+//    private static XSSFSheet ExcelWSheet;
+//    private static XSSFWorkbook ExcelWBook;
+//    private static XSSFCell Cell;
+//    private static XSSFRow Row;
+    WebDriver driver;
+//    WebDriverWait wait;
     //private List<String> List = new ArrayList<String>();
     String url = "http://thedemosite.co.uk ";
+    ExtentReports report;
+    ExtentTest test;
 
 
-  @BeforeClass
-    public void init(){
-      System.out.println("Run Before SetUp");
-
-  }
+//  @BeforeClass
+//    public void init(){
+//
+//      System.out.println("Run Before SetUp");
+//
+//  }
     @BeforeTest
     public void setUp(){
+        report = new ExtentReports("C:/Users/Admin/Desktop/AT12/AutoReport.html", true);
+        test = report.startTest("Verify Add and Save User");
         System.setProperty("webDriver.chromeDriver", "C:/Users/Admin/Desktop/AT12/chromedriver.exe");
         driver = new ChromeDriver();
         //wait = new WebDriverWait(driver, 5);
-        driver.get(url);
         driver.manage().window().maximize();
+        test.log(LogStatus.INFO, "Browser started");
+        driver.get(url);
 
     }
 
@@ -64,6 +68,12 @@ public class DemoSiteTest {
         driver.findElement(By.cssSelector("body > table > tbody > tr > td.auto-style1 > form > div > center > table > tbody > tr > td:nth-child(1) > div > center > table > tbody > tr:nth-child(2) > td:nth-child(2) > p > input[type=\'password\']")).sendKeys(password);
         Thread.sleep(1000);
         driver.findElement(By.cssSelector("body > table > tbody > tr > td.auto-style1 > form > div > center > table > tbody > tr > td:nth-child(1) > div > center > table > tbody > tr:nth-child(3) > td:nth-child(2) > p > input[type=\'button\']")).click();
+//
+//        Assert.assertEquals("saywhat", username);
+//        Assert.assertEquals("igotuboo", password);
+//
+//        report.endTest(test);
+//        report.flush();
 
     }
 
@@ -84,9 +94,15 @@ public class DemoSiteTest {
         driver.findElement(By.cssSelector("body > table > tbody > tr > td.auto-style1 > form > div > center > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(3) > td:nth-child(2) > p > input[type=\"button\"]")).click();
         Thread.sleep(1000);
 
-        Alert alert = driver.switchTo().alert();
-        String alertText = alert.getText();
-        assertEquals("New User Logged In!", alertText);
+//        Assert.assertEquals("saywhat", username);
+//        Assert.assertEquals("igotuboo", password);
+//
+//        report.endTest(test);
+//        report.flush();
+
+//        Alert alert = driver.switchTo().alert();
+//        String alertText = alert.getText();
+//        assertEquals("New User Logged In!", alertText);
 
 
     }
@@ -108,6 +124,12 @@ public class DemoSiteTest {
         driver.findElement(By.cssSelector("body > table > tbody > tr > td.auto-style1 > form > div > center > table > tbody > tr > td:nth-child(1) > table > tbody > tr:nth-child(3) > td:nth-child(2) > p > input[type=\"button\"]")).click();
         Thread.sleep(2000);
 
+        Assert.assertEquals("Godsblessing", username);
+        Assert.assertEquals("IsdaBest", password);
+
+        report.endTest(test);
+        report.flush();
+
         //Login attempt failed because this user was never created. Test passed.
 
     }
@@ -115,7 +137,17 @@ public class DemoSiteTest {
     @Test
     public void testLoginValidity() throws InterruptedException{
 
-        //assertTrue();
+        Assert.assertEquals("saywhat", username);
+        Assert.assertEquals("igotuboo", password);
+
+        testLoginValidity.testSuccessfulUserLogin();
+        testLoginValidity.testInvalidLoginNewUser();
+
+
+        report.endTest(test);
+        report.flush();
+
+        assertTrue();
 
     }*/
 
---
+++ src/test/java/TestDemo.java
@@ -0,0 +1,70 @@
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+//public class TestDemo {
+//
+//    WebDriver driver;
+//    String url = "http://thedemosite.co.uk";
+//    ExtentReports report;
+//    ExtentTest test;
+//
+//    @BeforeClass
+//    public void init(){
+//        report = new ExtentReports("C:/Users/Admin/Desktop/AT12/AutomationReport.html", true);
+//        test = report.startTest("Verify New User Addition");
+//    }
+//
+//    @Before
+//    public void setUp(){
+//        //System.setProperty("webDriver.Firefox", "C:/Users/Admin/Desktop/AT12/Web_Driver/geckodriver.exe");
+//        System.setProperty("webDriver.Firefox", "C:/Users/Admin/Desktop/AT12/Web_Driver/chromedriver.exe");
+//        driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        //WebDriverWait<WebDriver> wait = driver.;
+//        test.log(LogStatus.INFO, "Browser started");
+//        driver.get(url);
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+////
+//    }
+//
+//    @Test
+//    public void testCreateUser() throws InterruptedException{
+//        String username = "disadrag";
+//        String password = "Thisfault";
+//
+//        driver.findElement(By.cssSelector("body > div:nth-child(1) > center:nth-child(1) > table:nth-child(6) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(1) > div:nth-child(2) > center:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > p:nth-child(1) > small:nth-child(1) > a:nth-child(6)")).click();
+//        Thread.sleep(1000);
+//        driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[1]/td[2]/p/input")).sendKeys(username);
+//        driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[2]/td[2]/p/input")).sendKeys(password);
+//        Thread.sleep(1000);
+//        driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[3]/td[2]/p/input")).click();
+//        Thread.sleep(2000);
+//
+//        Assert.assertEquals("Please be right!", username, "disadrag");
+//        Assert.assertEquals("You got that right!", password, "Thisfault");
+//
+//        report.endTest(test);
+//        report.flush();
+//
+//    }
+//
+//    @After
+//    public void tearDown(){
+//        driver.quit();
+//    }
+////    @AfterClass
+//}
